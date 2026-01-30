423d59058e51ace84d371ec516cbbb9e1a2c3f81
marcee <marceloquadrilatero@gmail.com>
2026-01-30 20:53:10 -0300

    add logica reservas

--- vite-project/src/ui/GestionReservas.jsx
+++ vite-project/src/ui/GestionReservas.jsx
@@ -1,12 +1,26 @@
 import { useAppContext } from "../context/UseAppContext";
-import "../styles/pages/gestionReservas.css"
+import { AiTwotoneDelete } from "react-icons/ai";
+import { FaHandshake } from "react-icons/fa6";
+import { MdEdit } from "react-icons/md";
+import "../styles/pages/gestionReservas.css";
 export const GestionReservas = () => {
   const { reservas, setReservas } = useAppContext();
   return (
     <section className="gestion-reservas">
       <div className="gestion-reservas__cont">
         {reservas.map((reserva, index) => (
-            <p key={index}>{reserva.nombre} {reserva.apellido} {reserva.telefono} {reserva.modelo} {reserva.seña}</p>
+          <div className="gestion-reservas__cont-reserva">
+            <p key={index}>
+              {reserva.nombre} {reserva.apellido} {reserva.telefono}{" "}
+              {reserva.modelo} {reserva.seña}
+            </p>
+            <div className="gestion-reservas__cont-icons">
+
+            <AiTwotoneDelete />
+            <FaHandshake />
+            <MdEdit />
+            </div>
+          </div>
         ))}
       </div>
     </section>
